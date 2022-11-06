43be4b9daa5656ce8f9fbef5bf5c652307f689a0
Renato Semadeni <renato.semadeni@gmail.com>
2022-11-06 21:23:35 +0100

    feat: v7 -> make POST request

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -16,6 +17,17 @@ func main() {
 		mux := http.NewServeMux()
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("server: %s /\n", r.Method)
+			fmt.Printf("server: query id: %s\n", r.URL.Query().Get("id"))
+			fmt.Printf("server: content-type: %s\n", r.Header.Get("content-type"))
+			fmt.Printf("server: headers:\n")
+			for headerName, headerValue := range r.Header {
+				fmt.Printf("\t%s = %s\n", headerName, strings.Join(headerValue, ", "))
+			}
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Printf("server: could not read request body: %s\n", err)
+			}
+			fmt.Printf("server: request body: %s\n", reqBody)
 			fmt.Fprintf(w, `{"message": "hello!"}`)
 		})
 		server := http.Server{
