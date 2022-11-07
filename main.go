42a9c20333d9786ee0443d222c6e04c7dbcecece
Renato Semadeni <renato.semadeni@gmail.com>
2022-11-07 19:05:51 +0100

    feat: v8 -> static fileserver

--- main.go
+++ main.go
@@ -1,76 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
-const serverPort = 3333
-
 func main() {
-	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Printf("server: %s /\n", r.Method)
-			fmt.Printf("server: query id: %s\n", r.URL.Query().Get("id"))
-			fmt.Printf("server: content-type: %s\n", r.Header.Get("content-type"))
-			fmt.Printf("server: headers:\n")
-			for headerName, headerValue := range r.Header {
-				fmt.Printf("\t%s = %s\n", headerName, strings.Join(headerValue, ", "))
-			}
-			reqBody, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("server: could not read request body: %s\n", err)
-			}
-			fmt.Printf("server: request body: %s\n", reqBody)
-			fmt.Fprintf(w, `{"message": "hello!"}`)
-		})
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", serverPort),
-			Handler: mux,
-		}
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("error running http server: %s\n", err)
-			}
-		}
-	}()
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
-	time.Sleep(100 * time.Millisecond)
-
-	jsonBody := []byte(`{"client_message": "hello, server!"}`)
-	bodyReader := bytes.NewReader(jsonBody)
-
-	requestURL := fmt.Sprintf("http://localhost:%d?id=4211", serverPort)
-	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	log.Print("Listening on: 3000...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
-		fmt.Printf("client: could not create request: %s\n", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	req.Header.Set("Content-Type", "application/json")
-	client := http.Client{
-		Timeout: 30 * time.Second,
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Client: error making http request: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
-
-	resBody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("client: could not read response body: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("client: response body: %s\n", resBody)
 }
