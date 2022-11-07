6e1786c1d4bbe511ab68ff9c7a26bf9cf44a26a9
Renato Semadeni <renato.semadeni@gmail.com>
2022-11-07 21:15:54 +0100

    feat: serving static sites

--- main.go
+++ main.go
@@ -3,15 +3,53 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", serveTemplate)
 
 	log.Print("Listening on: 3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	// return a 404 if the template doesn't exist
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+	//return a 404 if the request is for a directory
+	if info.IsDir() {
+		http.NotFound(w, r)
+		http.NotFound(w, r)
+		return
+	}
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		// log the detailed error
+		log.Print(err.Error())
+		// return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(500), 500)
 	}
 }
